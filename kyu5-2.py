2a62e8d1d9dad3f408bf5857caeb65a560624436
Lukasz Mozdzierz <44287347+KasuLVita@users.noreply.github.com>
2024-04-22 18:58:55 +0100

    Add files via upload

---
+++ kyu5-2.py
@@ -0,0 +1,25 @@
+'''
+*creator LektorLuigi
+------------
+04.22.2024
+Codewars Kyu 5
+'''
+
+n = int(input())
+def product_fib(prod):
+    fib_n = [0, 1]
+    if prod == 0:
+        return [0, 1, True]
+    a = fib_n[0]
+    b = fib_n[1]
+    while True:
+        c = a + b
+        a = b
+        b = c
+        if a * b >= prod:
+            if a * b == prod:
+                return [a, b, True]
+            else:
+                return [a, b, False]
+
+print(product_fib(n)) #e.g n = 714 -> True
